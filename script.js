fa20a83ba41e3ba5aabb3b4b638c1aeb6a1690d8
NeamaAli96 <159647951+NeamaAli96@users.noreply.github.com>
2024-02-10 20:28:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,437 @@
+// let a = 21;
+// let b = 20;
+
+// console.log(a , ("20"+"21") , ("20"+"21") , ("20"+"21") , b ); // _21_2021_2021_2021_20_
+// // Replace ? With Arithmetic Operators
+// console.log(!10 * (20 + 15 + 3 + 190 + 10 + 400)); // 0
+// let num = 3;
+
+// // Solution One
+// console.log(2*num); // 6
+
+// // Solution Two
+// console.log(3+num); // 6
+
+// // Soultion Three
+// if (num > 1){
+//     console.log(6)} // 6
+
+// // Soultion Four
+// if (num <  9){
+//     console.log(6)} // 6 // 6
+
+// // Solution Five
+// if (num =  6 ){
+//     console.log(6)}; // 6
+
+// // Solution Six
+// if (num !=  7 ){
+//     console.log(6)}; // 6
+// let num = "10";
+
+// // Solution One
+// console.log( +(num + num)); // 20
+
+// // Solution Two
+// console.log(+(num) + +(num)); // 20
+
+// // Solution Three
+// console.log("Write Your Code Here"); // 20
+
+// // Solution Four
+// console.log("Write Your Code Here"); // 20
+// let points = 10;
+
+// // Write Your Code Here
+
+// console.log(points + 3); // 13
+
+// // Write Your Code Here
+
+// console.log(points -2); // 8;
+// Examples
+// console.log(100_000); // 100000
+// console.log(100000); // 100000
+// console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+// console.log(1000*100); // 100000
+// console.log(10_00_00); // 100000
+// console.log(10000*10); // 100000
+// console.log(10**5); // 100000
+// let x = Number.MAX_SAFE_INTEGER;
+// let z = Number.MIN_SAFE_INTEGER;
+// console.log(x + z + 16)
+// var x =" 100.56789 Views";
+
+
+// console.log(parseInt (x)); // 100
+// console.log(parseFloat (x)); // 100
+// console.log(); // 100.57
+// let num = 10;
+
+// console.log(Number.isInteger(num)+1); // 2
+
+// let flt = 10.4;
+
+// console.log(Math.floor(flt)/ 2); // 10
+// console.log(Math.floor(flt)- 5); // 10
+// console.log(); // 10
+// console.log(); // 10
+// console.log(); // 10
+// console.log(); // 10
+// let x ="Elzero Web School"
+// let z="h"
+// console.log(x.charAt(2) , x.slice(3 ,6)  )
+// console.log(x.charAt(13 )  )
+// console.log(x.split( 2) )
+//  let x ="Elzero"
+//  console.log( x.substring(0)  )
+// //  console.log(x.repeat(2))
+// //  console.log()
+// //  console.log()
+// //  console.log()
+// //  console.log()
+// let word = "Elzero";
+// let letterZ = "z";
+// let letterE = "e";
+// let letterO = "O";
+
+// console.log(word.includes("z")); // True
+// console.log(word.startsWith("E")); // True
+// console.log(word.endsWith("o")); // True
+// console.log(100 == "100"); // true
+// console.log(100 < 1000); // true
+// console.log(110 > 100 && 10 < 20); // true
+// console.log(10 > "-10"); // true
+// console.log(! -50 || +"-40");  // true
+// console.log(! 10 !== -"-40"); // true
+// console.log( -"10" !== 10); // true
+// console.log(- 20 < false); // true
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log(num1 < num2 ); // true
+// console.log(num1 <= num2 ); // true
+// console.log(num2 > num1 ); // true
+// console.log(num2 >= num1 ); // true
+// console.log( num1 !== num2 ); // true
+// console.log(num1 <= num2 ); // true
+// let a = 20;
+// let b = 30;
+// let c = 10;
+
+// console.log(a < b && a > c || a >= b); // true
+// console.log(a + b > a + c); // true
+// console.log(!(a + b) && !(a + b) && !(a + c) && !(a + c)); // true
+// Test Case 1
+// let num1 = 9;
+// let num2 = 20;
+// let num3 = 110;
+// let x=10
+// let y=90
+//  if (num1 == x){
+// console.log("009")
+//  } else if(num1 > x && num1 < y){
+//     console.log("020")
+//  } else if(num3 >= 100){
+//     console.log(num3)
+//  }
+ 
+// // Test Case 2
+//  // "020"
+
+// // Test Case 3
+//  // "110"
+// let gob ="manager";
+// let salery = 8000;
+// let money =0
+
+// let holiday = prompt("enter your holiday")
+
+// switch(holiday){
+//    case 0: 
+//    money = 500;
+//     console.log(`my money is${money}`)
+//     break;
+//    case 1:
+//    case 2:
+//     money = 300 ;
+//     console.log(`my money is ${money}`)
+//     break;
+// }
+// if(holiday==0){
+// money= 8000
+// }else if(holiday == 2){
+//     money=7500
+// }else if(holiday == 9){
+//     money=5500
+// }else{
+//     money=" 0 $"
+// }
+// document.write( `your money is${money}`)
+// let num1 = 9;
+// let str = "9";
+// let str2 = "20";
+
+// if(num1 == str){
+
+// }else if(num1 !== str){
+
+// }
+// document.write(`${num1} Is The Same Value As ${str}`)
+// console.log(`${num1} Is The Same Value As ${str} But Not The Same Type`)
+// // Output
+
+// ""
+// "{num1} Is Not The Same Value Or The Same Type As {str2}"
+// "{str} Is The Same Type As {str2} But Not The Same Value"
+// let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
+// let num = 3;
+
+// // Method 1
+// console.log(myFriends);// ["Ahmed", "Elham", "Osama"];
+
+// myFriends=["Ahmed", "Elham", "Osama"]
+
+// console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
+
+// // Method 2
+// // console.log("Your Code Here"); // ["Ahmed", "Elham", "Osama"];
+// let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
+
+// // Write Your Code Here
+
+// console.log(friends.shift()); // ["Eman", "Osama"]
+// let arrOne = ["C", "D", "X"];
+// let arrTwo = ["A", "B", "Z"];
+// let finalArr = [];
+
+// // Write One Single Line Of Code
+// console.log(finalArr.concat(arrOne , arrTwo).sort().reverse())
+// let website = "Go";
+// let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
+
+// console.log(words.unshift()); // ZERO
+// let needle = "js";
+// let haystack = ["PHP", "JS", "Python"];
+ 
+//  if(haystack=("Js")){
+// console.log( "that found arrey")
+//  }
+// // Write 3 Solutions
+// let arr1 = ["A", "C", "X"];
+// let arr2 = ["D", "E", "F", "Y"];
+// let allArrs = [];
+
+// Your Code Here
+// (7) ['A', 'C', 'X', 'D', 'E', 'F', 'Y']
+// console.log(allArrs.concat(arr1 , arr2).slice(5 , -1).concat(arr1).slice(-1)); // fxy
+// let  x =prompt("Enter your DEg")
+// switch(x){
+//     case "a": 
+//         document.write("Exlent")
+//         break;
+//     case "b": 
+//        document.write("good")
+//         break;
+//     case "c": 
+//        document.write("fail")
+//         break;
+
+// }
+// let x=document.getElementById("div1") ;
+// let y =document.getElementById("btn1");
+// let z=0;
+// function fun(){
+//     x.innerHTML="hello bro"
+   
+// }
+// y.onclick=fun; 
+
+// window.onload=function(){
+//     alert("اقفل الصفحه بسرعه")
+// }
+
+// for(x=0 ; x<=30 ; x++ ){
+//     document.write (x)
+// }
+// let tit=("elzero");
+// let desc =("elzero web school");
+// let date =("25/12")
+
+// let x=   `
+// <div class="div">
+// <h1>${tit}</h1>
+// <h2>${desc}</h2>
+// <p>${date}</p>
+// </div>
+// `
+// document.write(x)
+// let a= 1_00 ;
+// let b= 2_00.5 ;
+// let c= 1e2 ;
+// let d= 2.4 ;
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// // the samlest var is (d)
+// document.write(Number.isInteger(a))
+// document.write(Number.isInteger(b))
+// document.write(Number.isInteger(c))
+// document.write(Number.isInteger(d))
+// console.log(Math.trunc(a),(b) ,(c), (d))
+// console.log(Math.round(d) *  (a**2))
+// console.log(Math.round(b))
+// console.log(Math.floor(b))
+// console.log(Math.trunc(b))
+// console.log(Math.ceil(b))
+// console.log((b) + (d)-(Math.floor(b , d))*(Math.pow(d,3)))
+// console.log(0xff)
+// let a = 10;
+// let b = "20";
+// let c = 80;
+// let x = a + b + c
+// console.log(+c++)
+// console.log(+a++)
+
+// // 11 + 21 + 81 - 11
+//// Replace ? With Arithmetic Operators
+// console.log(!(10 + 20 + 15 + 3 + 190 + 10) * 400); // 0
+// let num = "10";
+
+// // Solution One
+// console.log(+num + +num); // 6
+
+// // Solution Two
+// console.log(+num + +num++ ); // 6
+
+// // Soultion Three
+// console.log(); // 6
+
+// // Soultion Four
+// console.log(Math.pow(+ num, 3)); // 6
+
+// // Solution Five
+// console.log("Write Your Code Here"); // 6
+
+// // Solution Six
+// console.log("Write Your Code Here"); // 6
+// let points = 10;
+// let x = 3 ;
+// let z = -2 ;
+
+// // Write Your Code Here
+
+// console.log(points + x ); // 13
+
+// // Write Your Code Here
+
+// console.log(points + z); // 8;
+
+// let allProduct = document.querySelectorAll(".product li") ;
+// let div1 = document.querySelector("#div1");
+// let btn = document.querySelector("#showPrice");
+// let totalPrice = 0;
+// // /////////////////////
+
+// allProduct.forEach(function(item){
+//     item.onclick =function(){
+//         totalPrice+=+(item.getAttribute("price"))
+//         div1.innerHTML+=item.textContent + " "
+    //     if(div1.innerHTML !=""){
+    //     btn.style.display="block"
+    // }
+//     }
+    
+// })
+// btn.onclick=function(){
+//     console.log(totalPrice)
+// }
+// //////////////////////////////////////////////////////////////////////////////
+// let allProdact =document.querySelectorAll(".container h5");
+// let div1 =document.querySelector("#div1");
+// let btn1=document.querySelector("#showPrice");
+// let btn2 = document.querySelector("#btn2");
+// let totalPrice =3000;
+
+
+//  allProdact.forEach(function(item){
+    
+//     item.onclick =function(){
+//         totalPrice+= +(item.getAttribute("price"))
+//         div1.innerHTML+=item.textContent +"+"
+//         if(div1.innerHTML !=""){
+//             btn1.style.display="block"
+//         }
+
+//     }
+//  })
+ 
+//  btn1.onclick=function(){
+//       console.log(totalPrice)
+//      }
+// ///////////////////////////////////////////////////////////////////////////////////
+//      const array = [1,1,2];
+// const ruselt = [...new Set(array)];
+// console.log(ruselt);
+
+// const array2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// const ruselt1 = [...new Set(array2)];
+// console.log(ruselt1);
+
+
+// for(var x =0 ; x<= 0 ; x++){
+//     console.log(x)
+// }
+// // let salery=prompt("enter your salery")
+// // let month =0
+// // if(month==30){
+// //     salery= 8000
+// //     }else if(month == 25){
+// //        salery=5500
+// //     }else if(month == 20){
+// //        salery=4000
+// //     }else{
+// //       salery=" 0 $"
+// //     }
+// //     console.log( `your money is${salery}`)
+
+// let salery= prompt("budget?");
+
+// switch (salery) {
+//   case 1:
+//    console.log("high budget");
+//     break;
+//   case 2:
+//     console.log("nice budget");
+//     break;
+//   case 3:
+//     console.log("not enough budget");
+//     break;
+//   default:
+//     console.log("low budget");
+// }
+var allProducts = document.querySelectorAll(".products h5");
+var div1 = document.querySelector("#div1");
+var btn1 = document.querySelector("#showPrice");
+var btn2 =document.querySelector("#btn2")
+var totalPrice = 0 ;
+
+allProducts.forEach(function (item) {
+    item.onclick = function (){
+        totalPrice+=+(item.getAttribute("price"))
+        div1.innerHTML += item.textContent + " "
+
+        if (div1.innerHTML != ""){
+            btn1.style.display = "block"
+           
+        }
+
+    }
+})
+
+btn1.onclick = function (){
+    btn2.innerHTML=(totalPrice )
+}
